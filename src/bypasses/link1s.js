000d6b21e0bf63f297daec167301ca53a7feb68f
supernova <76390419+driedpampas@users.noreply.github.com>
2023-10-04 18:58:34 +0300

    add link1s bypass

---
+++ src/bypasses/link1s.js
@@ -0,0 +1,17 @@
+import BypassDefinition from './BypassDefinition.js';
+
+export default class Link1s extends BypassDefinition {
+    constructor() {
+        super()
+        // custom bypass required bases can be set here
+    }
+
+    execute() {
+        window.setInterval = f => setTimeout(f,1)
+        this.helpers.awaitElement("#link1s", a => {
+            a.click();
+        });
+    }
+}
+
+export const matches = ['anhdep24.com']
