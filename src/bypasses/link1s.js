44f091839734e666ca48c511095a69082bb17bea
supernova <76390419+driedpampas@users.noreply.github.com>
2023-10-05 11:10:59 +0300

    link1s bypass

--- src/bypasses/link1s.js
+++ src/bypasses/link1s.js
@@ -9,14 +9,20 @@ export default class Link1s extends BypassDefinition {
     execute() {  
         window.setInterval = f => setTimeout(f,1)
         this.helpers.awaitElement("#link1s", a => {
-            a.click();
+            this.helpers.safelyNavigate(a.href);
         });
 
-        const btn6 = document.querySelector("#btn6");
-            if (btn6) {
-                btn6.click();
-        }
+        let scripts = document.getElementsByTagName('script');
+        let secondScript = scripts[scripts.length - 2];
+        // Get the script's source
+        let scriptSrc = secondScript.innerHTML;
+        // Find the first location.href in the script and get the value of location.href
+        let url = scriptSrc.split('location.href = "')[1].split('"')[0];
+        alert(url);
+        this.helpers.safelyNavigate(url);
     }
 }
+
+
 
 export const matches = ['https://anhdep24.com', 'https://hocbeauty.com']
